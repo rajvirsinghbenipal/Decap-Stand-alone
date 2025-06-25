94ebc484766437acb33cda6b89510d89d6af3f34
rajvirsinghbenipal <rajvirsinghbenipal@gmail.com>
2025-06-25 10:21:13 +0000

    more

--- api/auth.js
+++ api/auth.js
@@ -1,24 +1,26 @@
 const { authorizationCode } = require('simple-oauth2');
 
-const config = {
-  client: {
-    id: process.env.OAUTH_CLIENT_ID,
-    secret: process.env.OAUTH_CLIENT_SECRET
-  },
-  auth: {
-    tokenHost: 'https://github.com',
-    tokenPath: '/login/oauth/access_token',
-    authorizePath: '/login/oauth/authorize'
-  }
-};
-
+// This is the function that starts the login process
 module.exports = (req, res) => {
+  const config = {
+    client: {
+      id: process.env.OAUTH_CLIENT_ID,
+      secret: process.env.OAUTH_CLIENT_SECRET
+    },
+    auth: {
+      tokenHost: 'https://github.com',
+      tokenPath: '/login/oauth/access_token',
+      authorizePath: '/login/oauth/authorize'
+    }
+  };
+
   const client = new authorizationCode(config);
 
+  // We redirect the user to GitHub's authorization page
   const authorizationUri = client.authorizeURL({
-    redirect_uri: `https://YOUR_VERCEL_URL/api/callback`,
-    scope: 'repo,user',
-    state: '3(#0/!~'
+    redirect_uri: `https://${req.headers.host}/api/callback`,
+    scope: 'repo,user', // This scope allows Decap to read/write to the repo
+    state: '3(#0/!~' // A random string to prevent CSRF attacks
   });
 
   res.writeHead(302, { Location: authorizationUri });
--- api/callback.js
+++ api/callback.js
@@ -1,45 +1,38 @@
 const { authorizationCode } = require('simple-oauth2');
 
-const config = {
-  client: {
-    id: process.env.OAUTH_CLIENT_ID,
-    secret: process.env.OAUTH_CLIENT_SECRET
-  },
-  auth: {
-    tokenHost: 'https://github.com',
-    tokenPath: '/login/oauth/access_token',
-    authorizePath: '/login/oauth/authorize'
-  }
-};
-
+// This is the function that GitHub calls back to after the user logs in
 module.exports = async (req, res) => {
   const { code } = req.query;
-  const options = {
-    code,
+  const config = {
+    client: {
+      id: process.env.OAUTH_CLIENT_ID,
+      secret: process.env.OAUTH_CLIENT_SECRET
+    },
+    auth: {
+      tokenHost: 'https://github.com',
+      tokenPath: '/login/oauth/access_token',
+      authorizePath: '/login/oauth/authorize'
+    }
   };
+
   const client = new authorizationCode(config);
 
   try {
-    const accessToken = await client.getToken(options);
+    // We exchange the temporary code for a permanent access token
+    const accessToken = await client.getToken({ code });
     const token = accessToken.token.access_token;
 
+    // This HTML and script sends the token back to the Decap CMS window
     const response = `
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <meta charset="utf-8">
-        <title>Authorizing...</title>
-      </head>
-      <body>
-        <script>
-          window.opener.postMessage('authorization:github:success:${JSON.stringify({
-            token: token,
-            provider: 'github'
-          })}', '*')
-          window.close()
-        </script>
-      </body>
-      </html>
+      <!DOCTYPE html><html><head><meta charset="utf-8"><title>Authorizing...</title></head><body>
+      <script>
+        window.opener.postMessage('authorization:github:success:${JSON.stringify({
+          token: token,
+          provider: 'github'
+        })}', '*')
+        window.close()
+      </script>
+      </body></html>
     `;
 
     res.status(200).send(response);
